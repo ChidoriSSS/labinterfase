c57290cb7d493a5264cc908972130338e7cad8e4
ChidoriSSS <leonidnurdygin3@gmail.com>
2024-10-22 22:56:51 +0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,204 @@
+let tg = window.Telegram.WebApp;
+
+tg.expand();
+
+tg.MainButton.textColor = "#FFFFFF";
+tg.MainButton.color = "#2cab37";
+
+let item = "";
+
+let btn1 = document.getElementById("1");
+let btn2 = document.getElementById("2");
+let btn3 = document.getElementById("3");
+let btn4 = document.getElementById("4");
+let btn5 = document.getElementById("5");
+let btn6 = document.getElementById("6");
+let btn7 = document.getElementById("7");
+let btn8 = document.getElementById("8");
+let btn9 = document.getElementById("9");
+let btn10 = document.getElementById("10");
+let btn11 = document.getElementById("11");
+let btn12 = document.getElementById("12");
+let btn13 = document.getElementById("13");
+let btn14 = document.getElementById("14");
+let btn15 = document.getElementById("15");
+let btn16 = document.getElementById("16");
+
+btn1.addEventListener("click", function () {
+   if (tg.MainButton.isVisible) {
+	 tg.MainButton.hide();
+   }
+   else {
+	tg.MainButton.setText("Вывести рецепт Паста Карбонара");
+	item = "1";
+	tg.MainButton.show();
+   }
+});
+
+btn2.addEventListener("click", function () {
+   if (tg.MainButton.isVisible) {
+	 tg.MainButton.hide();
+   }
+   else {
+	tg.MainButton.setText("Вывести рецепт Том Ям");
+	item = "2";
+	tg.MainButton.show();
+   }
+});
+
+btn3.addEventListener("click", function () {
+if (tg.MainButton.isVisible) {
+tg.MainButton.hide();
+}
+else {
+tg.MainButton.setText("Вывести рецепт Ризотто с грибами");
+item = "3";
+tg.MainButton.show();
+}
+});
+
+btn4.addEventListener("click", function () {
+if (tg.MainButton.isVisible) {
+tg.MainButton.hide();
+}
+else {
+tg.MainButton.setText("Вывести рецепт Мясное рагу с овощами");
+item = "4";
+tg.MainButton.show();
+}
+});
+
+btn5.addEventListener("click", function () {
+if (tg.MainButton.isVisible) {
+tg.MainButton.hide();
+}
+else {
+tg.MainButton.setText("Вывести рецепт Суп Фо");
+item = "5";
+tg.MainButton.show();
+}
+});
+
+btn6.addEventListener("click", function () {
+if (tg.MainButton.isVisible) {
+tg.MainButton.hide();
+}
+else {
+tg.MainButton.setText("Вывести рецепт Курица Тика Масала");
+item = "6";
+tg.MainButton.show();
+}
+});
+
+btn7.addEventListener("click", function () {
+if (tg.MainButton.isVisible) {
+tg.MainButton.hide();
+}
+else {
+tg.MainButton.setText("Вывести рецепт Борщ");
+item = "7";
+tg.MainButton.show();
+}
+});
+
+btn8.addEventListener("click", function () {
+if (tg.MainButton.isVisible) {
+tg.MainButton.hide();
+}
+else {
+tg.MainButton.setText("Вывести рецепт Пад Тай");
+item = "8";
+tg.MainButton.show();
+}
+});
+
+btn9.addEventListener("click", function () {
+if (tg.MainButton.isVisible) {
+tg.MainButton.hide();
+}
+else {
+tg.MainButton.setText("Вывести рецепт Кускус с овощами");
+item = "9";
+tg.MainButton.show();
+}
+});
+
+btn10.addEventListener("click", function () {
+if (tg.MainButton.isVisible) {
+tg.MainButton.hide();
+}
+else {
+tg.MainButton.setText("Вывести рецепт Лазанья");
+item = "10";
+tg.MainButton.show();
+}
+});
+
+btn11.addEventListener("click", function () {
+if (tg.MainButton.isVisible) {
+tg.MainButton.hide();
+}
+else {
+tg.MainButton.setText("Вывести рецепт Плов");
+item = "11";
+tg.MainButton.show();
+}
+});
+
+btn12.addEventListener("click", function () {
+if (tg.MainButton.isVisible) {
+tg.MainButton.hide();
+}
+else {
+tg.MainButton.setText("Вывести рецепт Шашлык");
+item = "12";
+tg.MainButton.show();
+}
+});
+
+btn13.addEventListener("click", function () {
+if (tg.MainButton.isVisible) {
+tg.MainButton.hide();
+}
+else {
+tg.MainButton.setText("Вывести рецепт Запечённый картофель с сыром");
+item = "13";
+tg.MainButton.show();
+}
+});
+
+btn14.addEventListener("click", function () {
+if (tg.MainButton.isVisible) {
+tg.MainButton.hide();
+}
+else {
+tg.MainButton.setText("Вывести рецепт Мусака");
+item = "14";
+tg.MainButton.show();
+}
+});
+
+btn15.addEventListener("click", function () {
+if (tg.MainButton.isVisible) {
+tg.MainButton.hide();
+}
+else {
+tg.MainButton.setText("Вывести рецепт Фахитас");
+item = "15";
+tg.MainButton.show();
+}
+});
+
+btn16.addEventListener("click", function () {
+if (tg.MainButton.isVisible) {
+tg.MainButton.hide();
+}
+else {
+tg.MainButton.setText("Вывести рецепт Курица Тика Масала");
+item = "16";
+tg.MainButton.show();
+}
+});
+Telegram.WebApp.onEvent("mainButtonClicked", function () {
+tg.sendData(item);
+});
